516402764e831489e76214153306a61d341b96f0
Rudolf Turoň <54534256+RudasCZ@users.noreply.github.com>
2025-04-23 08:08:53 +0200

    Initial commit

---
+++ src/main/java/com/homework/morosystems/MorosystemsApplication.java
@@ -0,0 +1,13 @@
+package com.homework.morosystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorosystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorosystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/morosystems/MorosystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.morosystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorosystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
